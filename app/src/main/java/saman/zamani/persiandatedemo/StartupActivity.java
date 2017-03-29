518b2f1a45328fbfda8280d4d2113c56f1c8210d
Saman Zamani <saman.zamani1@gmail.com>
2017-03-29 16:04:23 +0430

    Make Date file

---
+++ app/src/main/java/saman/zamani/persiandatedemo/StartupActivity.java
@@ -0,0 +1,17 @@
+package saman.zamani.persiandatedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Saman on 3/29/2017 AD.
+ */
+
+public class StartupActivity extends Activity
+{
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
