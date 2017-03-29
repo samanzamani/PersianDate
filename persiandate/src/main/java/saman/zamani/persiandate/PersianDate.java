d4463207275afa01c646d64087264a0e06490d76
Saman Zamani <saman.zamani1@gmail.com>
2017-03-29 15:38:21 +0430

    Make Date file

---
+++ persiandate/src/main/java/saman/zamani/persiandate/PersianDate.java
@@ -0,0 +1,15 @@
+package saman.zamani.persiandate;
+
+import java.util.Date;
+
+/**
+ * Created by Saman on 3/29/2017 AD.
+ */
+
+public class PersianDate
+{
+	private Date date;
+	public PersianDate(){
+
+	}
+}
