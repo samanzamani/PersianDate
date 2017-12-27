81d6cd428156a93e21c716158c4fa472ab5d8dc4
Saman Zamani <saman.zamani1@gmail.com>
2017-12-27 17:17:39 +0330

    Add new branch

---
+++ persiandate/src/main/java/saman/zamani/persiandate/GSPCalendar.java
@@ -0,0 +1,19 @@
+package saman.zamani.persiandate;
+
+import java.util.Date;
+
+/**
+ * Created by Saman on 12/27/2017 AD.
+ */
+
+public class GSPCalendar {
+  private int GPSWeek;
+  private int GPSSecond;
+  public GSPCalendar(Date date){
+
+  }
+  public GSPCalendar(PersianDate date){
+
+  }
+
+}
