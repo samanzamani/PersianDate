8f0d364b17902e5dfdb419c9898ae5484240754d
Saman <saman.zamani1@gmail.com>
2023-09-08 16:40:02 +0330

    Remove Butterknift due to not supprt JDK 17

--- app/src/main/java/saman/zamani/persiandatedemo/AgeCalculator.java
+++ app/src/main/java/saman/zamani/persiandatedemo/AgeCalculator.java
@@ -1,98 +1,78 @@
 package saman.zamani.persiandatedemo;
 
-import android.content.Intent;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import com.google.android.material.navigation.NavigationView;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatRadioButton;
-import androidx.appcompat.widget.Toolbar;
-import android.view.Gravity;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.List;
-
-import butterknife.BindFont;
-import butterknife.BindView;
-import butterknife.BindViews;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Setter;
-import butterknife.ViewCollections;
-import saman.zamani.persiandate.PersianDate;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import saman.zamani.persiandate.PersianDateFormat;
 
 /**
  * Created by Saman on 4/12/2017 AD.
  */
 
-public class AgeCalculator extends AppCompatActivity
-{
-	private PersianDateFormat formater = new PersianDateFormat("l j F Y");
-	@BindFont(R.font.byekan)
+public class AgeCalculator extends AppCompatActivity {
+
+  private PersianDateFormat formater = new PersianDateFormat("l j F Y");
+  /*
+  @BindFont(R.font.byekan)
     Typeface bYekan;
-	@BindView(R.id.drawer_layout)
-	DrawerLayout drawer;
-	@BindView(R.id.nav_view)
-	NavigationView navView;
-	@BindView(R.id.top_bar)
-	Toolbar top_bar;
-	@BindView(R.id.btn_calc)
-	Button btnCalc;
-	@BindView(R.id.rd_togrg)
-	AppCompatRadioButton rdTogrg;
-	@BindView(R.id.rd_tojalali)
-	AppCompatRadioButton rdTojalali;
-	@BindViews({R.id.edt_day,R.id.edt_month,R.id.edt_year})
-	List<EditText> editTexts;
-	@BindViews({R.id.txt_result,R.id.txt_day,R.id.txt_month,R.id.txt_year,R.id.txt_to_show,R.id.ageCalc,R.id.txt_to_grg,R.id.txt_to_jalali,R.id.txt_to_grg2,R.id.txt_to_jalali2,R.id.txt_title})
-	List<TextView> textViews;
+  @BindView(R.id.drawer_layout)
+  DrawerLayout drawer;
+  @BindView(R.id.nav_view)
+  NavigationView navView;
+  @BindView(R.id.top_bar)
+  Toolbar top_bar;
+  @BindView(R.id.btn_calc)
+  Button btnCalc;
+  @BindView(R.id.rd_togrg)
+  AppCompatRadioButton rdTogrg;
+  @BindView(R.id.rd_tojalali)
+  AppCompatRadioButton rdTojalali;
+  @BindViews({R.id.edt_day,R.id.edt_month,R.id.edt_year})
+  List<EditText> editTexts;
+  @BindViews({R.id.txt_result,R.id.txt_day,R.id.txt_month,R.id.txt_year,R.id.txt_to_show,R.id.ageCalc,R.id.txt_to_grg,R.id.txt_to_jalali,R.id.txt_to_grg2,R.id.txt_to_jalali2,R.id.txt_title})
+  List<TextView> textViews;
+*/
+//  final Setter<TextView, Typeface> SET_FONT = (view, tf, index) -> view.setTypeface(tf);
 
-	final Setter<TextView, Typeface> SET_FONT = (view, tf, index) -> view.setTypeface(tf);
+  @Override
+  public void onBackPressed() {
+//		if(drawer.isDrawerOpen(Gravity.RIGHT)){
+//			drawer.closeDrawer(Gravity.RIGHT);
+//		}else{
+//		}
+    super.onBackPressed();
+  }
 
-	@Override
-	public void onBackPressed() {
-		if(drawer.isDrawerOpen(Gravity.RIGHT)){
-			drawer.closeDrawer(Gravity.RIGHT);
-		}else{
-			super.onBackPressed();
-		}
-	}
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_agecalc);
-		ButterKnife.bind(this);
-		Bundle bundle = getIntent().getExtras();
-		if(bundle != null){
-			if(bundle.getString("TYPE").equals("TO_JALALI")){
-				rdTogrg.setChecked(true);
-			}else{
-				rdTojalali.setChecked(true);
-			}
-		}
-		rdTojalali.setChecked(true);
-		//Set fonts
-		ViewCollections.set(textViews,SET_FONT,bYekan);
-		btnCalc.setTypeface(bYekan);
-		//toolbar
-		setSupportActionBar(top_bar);
-		top_bar.setTitle("");
-		setSupportActionBar(top_bar);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, top_bar, R.string.open, R.string.close);
-		drawer.setDrawerListener(toggle);
-
-		toggle.syncState();
-	}
-
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_agecalc);
+//    ButterKnife.bind(this);
+    Bundle bundle = getIntent().getExtras();
+    if (bundle != null) {
+      if (bundle.getString("TYPE").equals("TO_JALALI")) {
+//				rdTogrg.setChecked(true);
+      } else {
+//				rdTojalali.setChecked(true);
+      }
+    }
+//		rdTojalali.setChecked(true);
+    //Set fonts
+//		ViewCollections.set(textViews,SET_FONT,bYekan);
+//		btnCalc.setTypeface(bYekan);
+    //toolbar
+//		setSupportActionBar(top_bar);
+//		top_bar.setTitle("");
+//		setSupportActionBar(top_bar);
+//		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//				this, drawer, top_bar, R.string.open, R.string.close);
+//		drawer.setDrawerListener(toggle);
+//
+//		toggle.syncState();
+  }
+/*
 	@OnClick(R.id.rd_tojalali) void JalaliSelect(){
 		if(rdTogrg.isChecked()){
 			rdTogrg.setChecked(false);
@@ -151,4 +131,5 @@ public class AgeCalculator extends AppCompatActivity
 		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/samanzamani/PersianDate"));
 		startActivity(browserIntent);
 	}
+ */
 }
--- app/src/main/java/saman/zamani/persiandatedemo/StartupActivity.java
+++ app/src/main/java/saman/zamani/persiandatedemo/StartupActivity.java
@@ -1,80 +1,71 @@
 package saman.zamani.persiandatedemo;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import androidx.annotation.MainThread;
-import androidx.annotation.Nullable;
-import com.google.android.material.navigation.NavigationView;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatSpinner;
-import androidx.appcompat.widget.Toolbar;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
-import butterknife.BindFont;
-import butterknife.BindView;
-import butterknife.BindViews;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-import java.util.List;
+import androidx.annotation.MainThread;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import butterknife.Setter;
-import butterknife.ViewCollections;
 import saman.zamani.persiandate.PersianDate;
-import saman.zamani.persiandate.PersianDateFormat;
 
 /**
  * Created by Saman on 3/29/2017 AD.
  */
 
-public class StartupActivity extends AppCompatActivity
-{
-	private final String[] displayFormat = {"فرمت نمایش را انتخاب کنید","Y/m/d","l j F Y \n H:i:s","j F y","z روز از سال","s","H:i","l w:i:s"};
-	Handler h = new Handler();
-	private String pattern = "l j F Y \n H:i:s";
-	@BindFont(R.font.byekan)
-	Typeface bYekan;
-	@BindView(R.id.drawer_layout)
-	DrawerLayout drawer;
-	@BindView(R.id.nav_view)
-	NavigationView navView;
-	@BindView(R.id.top_bar)
-	Toolbar top_bar;
-	@BindView(R.id.spn_format)
-	AppCompatSpinner spnFormat;
-	@BindViews({R.id.txt_title,R.id.txt_date,R.id.txt_to_jalali,R.id.txt_to_grg,R.id.ageCalc,R.id.txt_to_show})
-	List<TextView> textViews;
+public class StartupActivity extends AppCompatActivity {
 
-	final Setter<TextView, Typeface> SET_FONT = (view, tf, index) -> view.setTypeface(tf);
+  private final String[] displayFormat = {"فرمت نمایش را انتخاب کنید", "Y/m/d", "l j F Y \n H:i:s",
+      "j F y", "z روز از سال", "s", "H:i", "l w:i:s"};
+  Handler h = new Handler();
+  private String pattern = "l j F Y \n H:i:s";
+  /*
+  @BindFont(R.font.byekan)
+  Typeface bYekan;
+  @BindView(R.id.drawer_layout)
+  DrawerLayout drawer;
+  @BindView(R.id.nav_view)
+  NavigationView navView;
+  @BindView(R.id.top_bar)
+  Toolbar top_bar;
+  @BindView(R.id.spn_format)
+  AppCompatSpinner spnFormat;
+  @BindViews({R.id.txt_title,R.id.txt_date,R.id.txt_to_jalali,R.id.txt_to_grg,R.id.ageCalc,R.id.txt_to_show})
+  List<TextView> textViews;
+*/
+//  final Setter<TextView, Typeface> SET_FONT = (view, tf, index) -> view.setTypeface(tf);
 
-	@Override
-	public void onBackPressed() {
+  @Override
+  public void onBackPressed() {
+		/*
 		if(drawer.isDrawerOpen(Gravity.RIGHT)){
 			drawer.closeDrawer(Gravity.RIGHT);
 		}else{
 			super.onBackPressed();
 		}
-	}
+		 */
+  }
 
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_startup);
-		ButterKnife.bind(this);
-		//toolbar
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_startup);
+    PersianDate pdate = new PersianDate().initJalaliDate(1401, 6, 31,10,16,25);
+    Log.i("LOG","date is " + pdate.toString());
+    pdate.subDays(36);
+    Log.i("LOG","date is " + pdate.toString());
+    Log.i("LOG","date is " + pdate.isToday());
+//		ButterKnife.bind(this);
+    //toolbar
+		/*
 		setSupportActionBar(top_bar);
 		top_bar.setTitle("");
 		setSupportActionBar(top_bar);
@@ -83,42 +74,42 @@ public class StartupActivity extends AppCompatActivity
 		drawer.addDrawerListener(toggle);
 		toggle.syncState();
 		ViewCollections.set(textViews,SET_FONT,bYekan);
-		new Timer().scheduleAtFixedRate(new TimerTask()
-		{
-			@Override
-			public void run() {
-				changeTime();
-			}
-		}, 0, 1000);//put here time 1000 milliseconds=1 second
-		//DO: Spinner
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_dropdown_item, displayFormat)
-		{
+		 */
+    new Timer().scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        changeTime();
+      }
+    }, 0, 1000);//put here time 1000 milliseconds=1 second
+    //DO: Spinner
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_spinner_dropdown_item, displayFormat) {
 
-			public View getView(int position, View convertView, ViewGroup parent) {
-				View v = super.getView(position, convertView, parent);
+      public View getView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
 
-				((TextView) v).setTypeface(bYekan);
-				if(position == 0){
-					((TextView) v).setTextColor(Color.parseColor("#c2c2c2"));
-				}else{
-					((TextView) v).setTextColor(Color.BLACK);
-				}
-				((TextView) v).setTextSize(14);
+//				((TextView) v).setTypeface(bYekan);
+        if (position == 0) {
+          ((TextView) v).setTextColor(Color.parseColor("#c2c2c2"));
+        } else {
+          ((TextView) v).setTextColor(Color.BLACK);
+        }
+        ((TextView) v).setTextSize(14);
 
-				return v;
-			}
+        return v;
+      }
 
 
-			public View getDropDownView(int position, View convertView, ViewGroup parent) {
-				View v = super.getDropDownView(position, convertView, parent);
+      public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View v = super.getDropDownView(position, convertView, parent);
 
-				((TextView) v).setTypeface(bYekan);
+//				((TextView) v).setTypeface(bYekan);
 
-				return v;
-			}
-		};
-		spnFormat.setAdapter(adapter);
+        return v;
+      }
+    };
+		/*
+//		spnFormat.setAdapter(adapter);
 		spnFormat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
 		{
 			@Override
@@ -134,33 +125,33 @@ public class StartupActivity extends AppCompatActivity
 
 			}
 		});
-	}
+		 */
+  }
 
-	@MainThread
-	private void changeTime() {
-		h.post(new Runnable()
-		{
-			@Override
-			public void run() {
-				textViews.get(1).setText(number2persian(new PersianDateFormat(pattern).format(new PersianDate())));
-			}
-		});
-	}
+  @MainThread
+  private void changeTime() {
+    h.post(new Runnable() {
+      @Override
+      public void run() {
+//				textViews.get(1).setText(number2persian(new PersianDateFormat(pattern).format(new PersianDate())));
+      }
+    });
+  }
 
-	private String number2persian(String text) {
-		text = text.replaceAll("0", "۰");
-		text = text.replaceAll("1", "۱");
-		text = text.replaceAll("2", "۲");
-		text = text.replaceAll("3", "۳");
-		text = text.replaceAll("4", "۴");
-		text = text.replaceAll("5", "۵");
-		text = text.replaceAll("6", "۶");
-		text = text.replaceAll("7", "۷");
-		text = text.replaceAll("8", "۸");
-		text = text.replaceAll("9", "۹");
-		return text;
-	}
-
+  private String number2persian(String text) {
+    text = text.replaceAll("0", "۰");
+    text = text.replaceAll("1", "۱");
+    text = text.replaceAll("2", "۲");
+    text = text.replaceAll("3", "۳");
+    text = text.replaceAll("4", "۴");
+    text = text.replaceAll("5", "۵");
+    text = text.replaceAll("6", "۶");
+    text = text.replaceAll("7", "۷");
+    text = text.replaceAll("8", "۸");
+    text = text.replaceAll("9", "۹");
+    return text;
+  }
+/*
 	@OnClick(R.id.txt_to_jalali) void startConvert(){
 		Intent intent = new Intent(StartupActivity.this, DateConverter.class);
 		intent.putExtra("TYPE","TO_JALALI");
@@ -181,4 +172,5 @@ public class StartupActivity extends AppCompatActivity
 		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/samanzamani/PersianDate"));
 		startActivity(browserIntent);
 	}
+ */
 }
